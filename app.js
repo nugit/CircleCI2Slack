88e2b6f21b66945803e0d76edf7a5d9f9cbba55e
Harper Reed <harper@nata2.org>
2013-10-05 17:43:19 -0500

    updated the app

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var requests = require('request');
 var app = express();
 app.use(express.logger());
 
@@ -6,9 +7,21 @@ app.get('/', function(request, response) {
     response.redirect('http://ltc.io')
 });
 
+console.log('before')
+
+console.log('after')
 
 app.get('/build', function(request, response) {
-    response.redirect('http://ltc.io')
+    response.send('Hello World!');
+    requests.post(
+        'http://dopeman.org/post/',
+        { form: { key: 'value' } },
+        function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                console.log(body)
+            }
+        }
+    );
 });
 
 var port = process.env.PORT || 5000;
