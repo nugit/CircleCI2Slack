5685fc636cfda2a974fc7378da8b3505f4666f77
Harper Reed <harper@nata2.org>
2013-10-05 19:52:30 -0500

    string management

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 var express = require("express");
 var qs = require('querystring');
 var requests = require('request');
@@ -14,13 +15,18 @@ post_handler = function(payload) {
     payload = payload['payload'];
     console.log(payload);
     console.log(JSON.stringify(payload));
-
-    console.log("Status: " + payload['status'])
-    console.log("Commit message: " + payload['subject'])
-    console.log("Committed by " + payload['author_name'] + " (" + payload['committer_email'] + ")" + " on ");
-    console.log("Branch: " + payload['branch'])
-    console.log("Commit url: " + payload['vcs_url'] + "/commit/" + payload['vcs_revision'])
-    console.log("Build time: " + payload['build_time_millis'] + " seconds")
+    n=payload['vcs_url'].replace("https://github.com/","").split('/');
+    user = n[0];
+    repo = n[1];
+    message_string = ""
+    message_string = message_string + "Status: " + payload['status']
+    message_string = message_string +  "Commit message: " + payload['subject']
+    message_string = message_string + "Committed by " + payload['author_name'] + " (" + payload['committer_email'] + ")" + " on " + payload['committer_date']
+    message_string = message_string + "Project: " + repo
+    message_string = message_string + "Branch: " + payload['branch']
+    message_string = message_string + "Commit url: " + payload['vcs_url'] + "/commit/" + payload['vcs_revision']
+    message_string = message_string + "Build time: " + payload['build_time_millis'] + " seconds"
+    console.log(message_string);
     requests.post('http://dopeman.org/post/', {form:payload});
 
 }
@@ -43,3 +49,9 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+*/
+
+ str="https://github.com/lunarcorp/api";
+ n=str.replace("https://github.com/","").split('/');
+user = n[0];
+repo = n[1];
