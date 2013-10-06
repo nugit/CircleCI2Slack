d13baef14c302632edeeafa1a0abd0762481d56a
Harper Reed <harper@nata2.org>
2013-10-05 19:59:44 -0500

    date parsing!

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+var dateFormat = require('dateformat');
 var express = require("express");
 var qs = require('querystring');
 var requests = require('request');
@@ -18,10 +18,15 @@ post_handler = function(payload) {
     n=payload['vcs_url'].replace("https://github.com/","").split('/');
     user = n[0];
     repo = n[1];
+    var date = new Date(payload['committer_date'] * 1000);
+
+    committer_date = dateFormat(date, "dddd, mmmm dS, yyyy, h:MM:ss TT");
+
+
     message_string = ""
     message_string = message_string + "Status: " + payload['status']
     message_string = message_string +  "Commit message: " + payload['subject']
-    message_string = message_string + "Committed by " + payload['author_name'] + " (" + payload['committer_email'] + ")" + " on " + payload['committer_date']
+    message_string = message_string + "Committed by " + payload['author_name'] + " (" + payload['committer_email'] + ")" + " on " + committer_date
     message_string = message_string + "Project: " + repo
     message_string = message_string + "Branch: " + payload['branch']
     message_string = message_string + "Commit url: " + payload['vcs_url'] + "/commit/" + payload['vcs_revision']
@@ -49,9 +54,3 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-*/
-
- str="https://github.com/lunarcorp/api";
- n=str.replace("https://github.com/","").split('/');
-user = n[0];
-repo = n[1];
