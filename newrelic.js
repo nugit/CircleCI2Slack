41923be0fe8fdb47f526595c0e18db120e0671db
Harper Reed <harper@nata2.org>
2014-01-06 11:30:20 -0600

    added newrelic config

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['Circle 2 Slack bot'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : 'b1fc095d57a42366ddb84e039c16f7e015b3dc65',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'trace'
+  }
+};
